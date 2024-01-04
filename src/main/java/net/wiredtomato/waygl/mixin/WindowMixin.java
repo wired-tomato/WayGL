c35514fce93209b0b3e442e15fe5a2363922da8e
moehreag <moehreag@gmail.com>
2024-01-04 02:17:28 +0100

    fix my code

--- src/main/java/net/wiredtomato/waygl/mixin/WindowMixin.java
+++ src/main/java/net/wiredtomato/waygl/mixin/WindowMixin.java
@@ -1,16 +1,14 @@
 package net.wiredtomato.waygl.mixin;
 
-import net.minecraft.SharedConstants;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.WindowEventHandler;
 import net.minecraft.client.WindowSettings;
-import net.minecraft.client.util.*;
+import net.minecraft.client.util.Icons;
+import net.minecraft.client.util.MonitorTracker;
+import net.minecraft.client.util.Window;
 import net.minecraft.resource.ResourcePack;
 import net.wiredtomato.waygl.IconInjector;
 import net.wiredtomato.waygl.WayGL;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,29 +17,21 @@ import static org.lwjgl.glfw.GLFW.*;
 
 @Mixin(Window.class)
 public abstract class WindowMixin {
-    @Shadow @Final private long handle;
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
-    public void waygl$addWindowHints(WindowEventHandler windowEventHandler, MonitorTracker monitorTracker, WindowSettings windowSettings, String string, String string2, CallbackInfo ci) {
-        if (WayGL.useWayland()) {
-            glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_FALSE);
-            IconInjector.INSTANCE.inject();
-            glfwWindowHintString(GLFW_WAYLAND_APP_ID, IconInjector.INSTANCE.getAPP_ID());
-        }
-    }
+	@Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
+	public void waygl$addWindowHints(WindowEventHandler windowEventHandler, MonitorTracker monitorTracker, WindowSettings windowSettings, String string, String string2, CallbackInfo ci) {
+		if (WayGL.useWayland()) {
+			glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_FALSE);
+			IconInjector.INSTANCE.inject();
+			glfwWindowHintString(GLFW_WAYLAND_APP_ID, IconInjector.APP_ID);
+		}
+	}
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetCursorEnterCallback(JLorg/lwjgl/glfw/GLFWCursorEnterCallbackI;)Lorg/lwjgl/glfw/GLFWCursorEnterCallback;"))
-    public void waygl$setIcon(WindowEventHandler windowEventHandler, MonitorTracker monitorTracker, WindowSettings windowSettings, String string, String string2, CallbackInfo ci) {
-        if (WayGL.useWayland()) {
-            glfwShowWindow(handle);
-            IconInjector.INSTANCE.setIcon(MinecraftClient.getInstance().getDefaultResourcePack(), SharedConstants.getGameVersion().isStable() ? Icons.RELEASE : Icons.SNAPSHOT);
-        }
-    }
-
-    @Inject(method = "setIcon", at = @At("HEAD"), cancellable = true)
-    public void waygl$cancelIcon(ResourcePack resourcePack, Icons icons, CallbackInfo ci) {
-        if (WayGL.useWayland()) {
-            ci.cancel();
-        }
-    }
+	@Inject(method = "setIcon", at = @At("HEAD"), cancellable = true)
+	private void waygl$setIcon(ResourcePack resourcePack, Icons icons, CallbackInfo ci) {
+		if (WayGL.useWayland()) {
+			IconInjector.INSTANCE.setIcon(resourcePack, icons);
+			ci.cancel();
+		}
+	}
 }
