c180cf4554ebf2107c7be05c39b1f5c90824a80b
wired-tomato <brokenfuse.self@gmail.com>
2025-05-17 15:40:12 -0400

    fix Version.kt

--- mod/common/src/main/kotlin/net/wiredtomato/waygl/util/Version.kt
+++ mod/common/src/main/kotlin/net/wiredtomato/waygl/util/Version.kt
@@ -9,16 +9,15 @@ data class Version(
     val additionalInfo: String
 ) {
     companion object {
-        val VERSION_PATTERN = Pattern.compile("([0-9]*).([0-9]*).([0-9]*)(.*)")
+        val VERSION_PATTERN = Pattern.compile("([0-9]+)\\.([0-9]+)(\\.([0-9]*)(.*))?")
 
         fun String.toVersion(): Version {
             val matcher = VERSION_PATTERN.matcher(this)
             matcher.find()
             val major = matcher.group(1).toInt()
             val minor = matcher.group(2).toInt()
-            val patch = matcher.group(3).toInt()
-            val additionalInfo = matcher.group(4)
-
+            val patch: Int = matcher.group(4)?.toInt() ?: 0
+            val additionalInfo: String = matcher.group(5) ?: ""
             return Version(major, minor, patch, additionalInfo)
         }
     }
